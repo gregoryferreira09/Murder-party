805a618a834f12ce944b7cc303df2249e599f927
gregoryferreira09 <gregory_ferreira@outlook.fr>
2025-05-17 14:17:44 +0200

    Update creer-partie.js

--- SRC/JS/creer-partie.js
+++ SRC/JS/creer-partie.js
@@ -1,12 +1,8 @@
-// Redirige vers la page de lancement de partie
-function genererScenario() {
-  window.location.href = "Lancement-partie.html";
-}
-
 // Affiche ou masque le champ "autre période"
 document.getElementById("periode").addEventListener("change", function () {
   document.getElementById("autrePeriode").style.display = (this.value === "autre") ? "block" : "none";
 });
+
 document.getElementById("genererBtn").addEventListener("click", function(e) {
   e.preventDefault();
 
