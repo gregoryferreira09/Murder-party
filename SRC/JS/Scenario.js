84486ee19c49c0deb4b3cce75f646661e6b5eb03
gregoryferreira09 <gregory_ferreira@outlook.fr>
2025-05-19 13:15:47 +0200

    Update Scenario.js

--- SRC/JS/Scenario.js
+++ SRC/JS/Scenario.js
@@ -142,7 +142,3 @@ document.addEventListener("DOMContentLoaded", () => {
     container.innerHTML = "Aucune donnée de scénario trouvée.";
   }
 });
-
-    container.innerHTML = "Aucune donnée de scénario trouvée.";
-  }
-});
