90c25c883541a578fc829689ceac8af0a387f497
gregoryferreira09 <gregory_ferreira@outlook.fr>
2025-05-20 11:41:58 +0200

    Create univers.js

---
+++ SRC/JS/univers.js
@@ -0,0 +1,16 @@
+// SRC/JS/Univers.js
+import alibis from './alibis.js';
+import indices from './indices.js';
+import personnages from './personnages.js';
+import lieux from './lieux.js';
+import armes from './armes.js';
+import { armeDescriptions } from './armeAction.js';
+
+export {
+  alibis,
+  indices,
+  personnages,
+  lieux,
+  armes,
+  armeDescriptions // ← Ajout ici
+};
